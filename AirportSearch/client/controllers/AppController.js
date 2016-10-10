9236b251b28776ae41b64517372bcad5d1649f9c
dhott <dhott@siue.edu>
2016-10-10 14:59:09 -0500

    Search by State
    Set up an api to pull all airports in a state through the GeoJSON file.
    Created a front-end to display search results through AngularJS.

---
+++ AirportSearch/client/controllers/AppController.js
@@ -0,0 +1,19 @@
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('AppController', ['$scope','$http', function($scope, $http){
+	console.log('AppController Initialized...');
+
+	$scope.getAirports = function(){
+		$http.get('/api/airports').success(function(response){
+			$scope.airports = response;
+		});
+	}
+
+	$scope.findAirports = function(){
+		$http.get('/api/airports/state/'+$scope.stateCode).success(function(response){
+			$scope.airports = response;
+		});
+	}
+
+
+}]);
