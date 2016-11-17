5de4c7acbca744313fcfbbbdf152976a09949708
dhott <dhott@siue.edu>
2016-11-16 18:18:14 -0600

    Template Engine II
    Added route to pass a variable from browser to template page.

--- TemplateEngines/app.js
+++ TemplateEngines/app.js
@@ -16,6 +16,12 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+app.get('/person/:id', function(req, res) {
+	res.render('person', { ID: req.params.id });
+});
 
+app.get('/api', function(req, res) {
+	res.json({ firstname: 'John', lastname: 'Doe' });
+});
 
 app.listen(port);
